3adfbda4a6d27f2009828ce30ca62e39139360f9
한승환 <coderrornotfound@gmail.com>
2025-09-20 23:11:56 +0900

    Create ReverseStr.py

---
+++ ReverseStr.py
@@ -0,0 +1,12 @@
+from ArrayStack import ArrayStack
+
+s = ArrayStack(50)
+
+msg = input("문자열 입력: ")
+for c in msg:
+    s.push(msg)
+
+print("문자열 출력: ", end="")
+while not s.isEmpty():
+    print(s.pop(), end="")
+print()
